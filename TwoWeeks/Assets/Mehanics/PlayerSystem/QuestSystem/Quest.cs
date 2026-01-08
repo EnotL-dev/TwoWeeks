3c2dff6ae54346f8803aea12bca4dc2fb0bdc3ff
EnotL-dev <enotyzer@gmail.com>
2026-01-08 18:55:25 +0300

    Quest system added
    quests

---
+++ TwoWeeks/Assets/Mehanics/PlayerSystem/QuestSystem/Quest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PlayerSystem.QuestSystem
+{
+    [CreateAssetMenu(fileName = "new quest", menuName = "Quest")]
+    public class Quest : ScriptableObject
+    {
+        public string description = "";
+    }
+}
